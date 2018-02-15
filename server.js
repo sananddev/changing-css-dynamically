a75512b104cd592fc46e0ea52274ac6cb19d1964
sandeep <kinarapari@gmail.com>
2018-02-15 12:32:42 +0545

    here is the first commit for the changing css dyanmaically app

--- server.js
+++ server.js
@@ -5,24 +5,20 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 //server listening at 127.0.0.1:3000
 server.listen(3000);
-console.log("Server listening at: 3000");
+console.log("Listening at 3000");
 //Handling the default route
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/css-color.html');
 });
-//Handling the route for admin
+//Handling the route for admin 
 app.get('/admin' , function(req,res) {
-    res.sendFile(__dirname + '/admin.html'); 
+    res.sendFile(__dirname + '/css-color-admin.html'); 
 })
 //Code for sockets
 io.on('connection', function (socket) {
-  
-  socket.emit('welcome', { data: 'welcome' });
-
-  socket.on('new' , function(data) {   
-          console.log('About to upload Quote')
-          io.sockets.emit( 'next' , { data : data } )
+  socket.on('admin' , function(data) {
+      console.log(data); 
+      console.log('about to broadcast')
+      io.sockets.emit( 'color' , { data : data } )
     })
 });
-
-								
